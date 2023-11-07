95c41c7177882b9cb4d10fcb623d7d151eae56c5
Kristina <kr.hranovska@gmail.com>
2023-11-07 15:11:19 +0200

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -3,9 +3,8 @@ const nameUser = document.querySelector('#name-output');
 
 const helloName = (event) => {
     nameUser.textContent = event.currentTarget.value.trim();
-    const hasSpaces = /\s/.test(nameUser.textContent);
 
-    if (!nameUser.textContent || hasSpaces) {
+    if (!nameUser.textContent) {
         nameUser.textContent = 'Anonymous';
     }
 }
